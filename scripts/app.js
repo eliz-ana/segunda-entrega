62a8974a4efa4c36da04ff4bb846e32940252efc
ana salas <anaprograma84@gmail.com>
2025-01-19 15:58:43 -0300

    agrege productos

--- scripts/app.js
+++ scripts/app.js
@@ -12,25 +12,81 @@ class Producto {
       (this.imagen = imagen); // Agregamos la URL de la imagen
   }
 }
-
-productos.push(
-  new Producto(
-    "Manzana",
-    160,
-    10,
-    "Fruta fresca y deliciosa",
-    "./imagenes/frutas.jpg"
-  )
-);
-productos.push(
-  new Producto(
-    "banana",
-    1700,
-    4,
-    "bananas del caribe",
-    "./../imagenes/frutas.jpg"
-  )
-);
+//productos
+const nuevosProductos = [
+  {
+    stock: 10,
+    nombre: "remera",
+    descripcion: "remera estampada",
+    precio: 7000,
+    imagen: "./imagenes/10remera.jpg",
+  },
+  {
+    stock: 10,
+    nombre: "remera",
+    descripcion: "remera gris",
+    precio: 7000,
+    imagen: "./imagenes/2remera-gris.jpg",
+  },
+  {
+    stock: 10,
+    nombre: "remera",
+    descripcion: "remera blanca",
+    precio: 6000,
+    imagen: "./imagenes/1camiseta-blanca.jpg",
+  },
+  {
+    stock: 10,
+    nombre: "camisa",
+    descripcion: "camisa blanca",
+    precio: 16000,
+    imagen: "./imagenes/3camisa-blanca.jpg",
+  },
+  {
+    stock: 10,
+    nombre: "pantalon",
+    descripcion: "jean claro",
+    precio: 26000,
+    imagen: "./imagenes/5pantalon-jean.jpg",
+  },
+  {
+    stock: 10,
+    nombre: "pantalon",
+    descripcion: "jean clasico",
+    precio: 25000,
+    imagen: "./imagenes/4pantalon-jean.jpg",
+  },
+  {
+    stock: 10,
+    nombre: "pantalon",
+    descripcion: "jean azul obscuro",
+    precio: 23000,
+    imagen: "./imagenes/9pantalonPortada.jpg",
+  },
+  {
+    stock: 10,
+    nombre: "bermuda",
+    descripcion: "bermuda beige ",
+    precio: 16000,
+    imagen: "./imagenes/7bermudaBeige.jpg",
+  },
+  {
+    stock: 10,
+    nombre: "bermuda",
+    descripcion: "bermuda negra",
+    precio: 18000,
+    imagen: "./imagenes/6bermuda.jpg",
+  },
+  {
+    stock: 10,
+    nombre: "camisa",
+    descripcion: "camisa azul",
+    precio: 16000,
+    imagen: "./imagenes/8camisa.jpg",
+  },
+];
+productos.push(...nuevosProductos);
+// render header
 const header = document.querySelector("#jsheader");
 header.innerHTML = `<nav class="navbar navbar-expand-lg bg-info-subtle">
   <div class="container-fluid">
@@ -52,7 +108,7 @@ header.innerHTML = `<nav class="navbar navbar-expand-lg bg-info-subtle">
     </div>
   </div>
 </nav>`;
-
+// render cards de productos
 function renderProductos(productos, containerid) {
   const containerCard = document.getElementById(containerid);
   containerCard.innerHTML = "";
@@ -61,12 +117,14 @@ function renderProductos(productos, containerid) {
     const card = document.createElement("div");
     card.classList.add("col-md-4");
     card.innerHTML = `
-    <div class="card d-flex" style="width: 18rem;">
-      <img src="${item.imagen}" class="card-img-top" alt="...">
+    <div class="card  img-fluid" style="width: 18rem;">
+      <img src="${item.imagen}" class="card-img-top img-fluid" alt="..."
+       style="height: 200px; object-fit: cover;">
       <div class="card-body">
         <h5 class="card-title">${item.nombre}</h5>
-        <p class="card-text">Some quick example text to build on the card title.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <p class="card-text">${item.descripcion}</p>
+        <p class="card-text">Precio: $${item.precio}</p>
+        <a href="./cart.html" class="btn btn-primary">Comprar</a>
       </div>
     </div>
     `;
