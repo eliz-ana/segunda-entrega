25c99f2ed111ec447d5600528d9661f5d1e669de
ana salas <anaprograma84@gmail.com>
2025-01-21 01:08:59 -0300

    datos recuperados

--- scripts/app.js
+++ scripts/app.js
@@ -3,36 +3,116 @@ console.log("hola");
 const productos = [];
 class Producto {
   static ultimoId = 1;
-  constructor(nombre, precio, cantidad, descripcion, imagen) {
+  constructor(nombre, precio, stock, cantidad, descripcion, imagen) {
     (this.id = Producto.ultimoId++),
       (this.nombre = nombre),
       (this.precio = precio),
+      (this.stock = stock),
       (this.cantidad = cantidad),
       (this.descripcion = descripcion), // Agregamos la descripción
       (this.imagen = imagen); // Agregamos la URL de la imagen
   }
 }
-
-productos.push(
-  new Producto(
-    "Manzana",
-    160,
-    10,
-    "Fruta fresca y deliciosa",
-    "./imagenes/frutas.jpg"
-  )
-);
-productos.push(
-  new Producto(
-    "banana",
-    1700,
-    4,
-    "bananas del caribe",
-    "./../imagenes/frutas.jpg"
-  )
-);
-const header = document.querySelector("#jsheader");
-header.innerHTML = `<nav class="navbar navbar-expand-lg bg-info-subtle">
+//productos
+const nuevosProductos = [
+  {
+    stock: 10,
+    nombre: "remera",
+    descripcion: "remera estampada",
+    precio: 7000,
+    imagen: "./imagenes/10remera.jpg",
+    cantidad: 1,
+  },
+  {
+    stock: 10,
+    nombre: "remera",
+    descripcion: "remera gris",
+    precio: 7000,
+    imagen: "./imagenes/2remera-gris.jpg",
+    cantidad: 1,
+  },
+  {
+    stock: 10,
+    nombre: "remera",
+    descripcion: "remera blanca",
+    precio: 6000,
+    imagen: "./imagenes/1camiseta-blanca.jpg",
+    cantidad: 1,
+  },
+  {
+    stock: 10,
+    nombre: "camisa",
+    descripcion: "camisa blanca",
+    precio: 16000,
+    imagen: "./imagenes/3camisa-blanca.jpg",
+    cantidad: 1,
+  },
+  {
+    stock: 10,
+    nombre: "pantalon",
+    descripcion: "jean claro",
+    precio: 26000,
+    imagen: "./imagenes/5pantalon-jean.jpg",
+    cantidad: 1,
+  },
+  {
+    stock: 10,
+    nombre: "pantalon",
+    descripcion: "jean clasico",
+    precio: 25000,
+    imagen: "./imagenes/4pantalon-jean.jpg",
+    cantidad: 1,
+  },
+  {
+    stock: 10,
+    nombre: "pantalon",
+    descripcion: "jean azul obscuro",
+    precio: 23000,
+    imagen: "./imagenes/9pantalonPortada.jpg",
+    cantidad: 1,
+  },
+  {
+    stock: 10,
+    nombre: "bermuda",
+    descripcion: "bermuda beige ",
+    precio: 16000,
+    imagen: "./imagenes/7bermudaBeige.jpg",
+    cantidad: 1,
+  },
+  {
+    stock: 10,
+    nombre: "bermuda",
+    descripcion: "bermuda negra",
+    precio: 18000,
+    imagen: "./imagenes/6bermuda.jpg",
+    cantidad: 1,
+  },
+  {
+    stock: 10,
+    nombre: "camisa",
+    descripcion: "camisa azul",
+    precio: 16000,
+    imagen: "./imagenes/8camisa.jpg",
+    cantidad: 1,
+  },
+];
+nuevosProductos.forEach((prod) => {
+  productos.push(
+    new Producto(
+      prod.nombre,
+      prod.precio,
+      prod.stock,
+      prod.cantidad,
+      prod.descripcion,
+      prod.imagen
+    )
+  );
+});
+// render header
+function renderHeader(params) {
+  const header = document.querySelector("#jsheader");
+  if (header) {
+    header.innerHTML = `<nav class="navbar navbar-expand-lg bg-info-subtle">
   <div class="container-fluid">
     <a class="navbar-brand" href="./index.html">Super Shop</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -52,26 +132,72 @@ header.innerHTML = `<nav class="navbar navbar-expand-lg bg-info-subtle">
     </div>
   </div>
 </nav>`;
+  } else {
+    console.log("el elemento con id jsheader no existe");
+  }
+}
 
+// render cards de productos
 function renderProductos(productos, containerid) {
   const containerCard = document.getElementById(containerid);
-  containerCard.innerHTML = "";
-  containerCard.classList.add("row", "g-3");
-  productos.forEach((item) => {
-    const card = document.createElement("div");
-    card.classList.add("col-md-4");
-    card.innerHTML = `
-    <div class="card d-flex" style="width: 18rem;">
-      <img src="${item.imagen}" class="card-img-top" alt="...">
+  if (containerCard) {
+    containerCard.innerHTML = "";
+    containerCard.classList.add("row", "g-3");
+    productos.forEach((item) => {
+      const card = document.createElement("div");
+      card.classList.add("col-md-4");
+      card.innerHTML = `
+    <div class="card  img-fluid" style="width: 18rem;">
+      <img src="${item.imagen}" class="card-img-top img-fluid" alt="..."
+       style="height: 200px; object-fit: cover;">
       <div class="card-body">
         <h5 class="card-title">${item.nombre}</h5>
-        <p class="card-text">Some quick example text to build on the card title.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <p class="card-text">${item.descripcion}</p>
+        <p class="card-text">Precio: $${item.precio}</p>
+        <button class="btn btn-outline-secondary add-to-cart" data-id="${item.id}">
+            Comprar
+        </button>
       </div>
-    </div>
+    </div>  
     `;
-    containerCard.appendChild(card);
-  });
+      containerCard.appendChild(card);
+    });
+  } else {
+    console.log(" el contenedor no existe ");
+  }
 }
+
+function cartSetLocalS(productos) {
+  //capturo cada product card
+  const containerCard = document.getElementById("jsCard");
+  if (containerCard) {
+    containerCard.addEventListener("click", (event) => {
+      if (event.target.classList.contains("add-to-cart")) {
+        const productId = event.target.getAttribute("data-id");
+
+        const product = productos.find((prod) => {
+          return prod.id === Number(productId);
+        });
+
+        if (product) {
+          let arrCart = JSON.parse(localStorage.getItem("cartValues")) || [];
+
+          // Agregar el producto al carrito
+          arrCart.push(product);
+          // Guardar el carrito actualizado
+          localStorage.setItem("cartValues", JSON.stringify(arrCart));
+          alert("producto agregado");
+        } else {
+          alert("no encontrado");
+        }
+      }
+    });
+  } else {
+    console.log("el contenedor jsCard no existe");
+  }
+}
+renderHeader();
 //llamo funcion pasando productos y el id del card-container
 renderProductos(productos, "jsCard");
+// guardar en local storage
+cartSetLocalS(productos);
---
+++ scripts/cart.js
@@ -0,0 +1,136 @@
+console.log("hola desde cart");
+// renderizacion del cart
+function renderCart(params) {
+  const productosCart = JSON.parse(localStorage.getItem("cartValues")) || [];
+
+  const containerCart = document.querySelector("#jsCartContainer");
+  if (containerCart) {
+    containerCart.innerHTML = "";
+
+    productosCart.forEach((item, index) => {
+      const subtotal = item.precio * item.cantidad;
+      const carrito = document.createElement("div");
+      carrito.classList.add("cart-div");
+      carrito.innerHTML = `
+       <div class="card mb-3" >
+          <div class="row g-0">
+            <div class="col-sm-4">
+              <img src="${item.imagen}" class="img-fluid rounded-start" alt="${item.nombre}">
+            </div>
+            <div class="col-sm-8">
+              <div class="card-body">
+                <h5 class="card-title">${item.nombre}</h5>
+                <p class="card-text">${item.descripcion}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <p class="card-text"><strong>Precio:</strong> $${item.precio}</p>
+                  <div class="cart-btns">
+                    <button class="btn btn-secondary btn-sm btn-restar" data-index="${index}">-</button>
+                    <span class="quantity">${item.cantidad}</span>
+                    <button class="btn btn-secondary btn-sm btn-sumar" data-index="${index}">+</button>
+                    <button class="btn btn-danger btn-sm btn-eliminar" data-index="${index}">Eliminar</button>
+                  </div>
+                  <p class="card-text"><strong>Subtotal:</strong> $<span class="subtotal">${subtotal}</span></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      `;
+      containerCart.appendChild(carrito);
+    });
+    setBotones();
+  }
+}
+//logica de los botones del cart
+function setBotones(params) {
+  const btnSumar = document.querySelectorAll(".btn-sumar");
+  const btnRestar = document.querySelectorAll(".btn-restar");
+  const btnEliminar = document.querySelectorAll(".btn-eliminar");
+  // funcion de suma
+  btnSumar.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const index = e.target.getAttribute("data-index");
+      const productoCart = JSON.parse(localStorage.getItem("cartValues")) || [];
+      productoCart[index].cantidad++;
+
+      localStorage.setItem("cartValues", JSON.stringify(productoCart));
+      renderCart();
+    });
+  });
+  // funcion restar
+  btnRestar.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const index = e.target.getAttribute("data-index");
+      const productoCart = JSON.parse(localStorage.getItem("cartValues") || []);
+      if (productoCart[index].cantidad > 1) {
+        productoCart[index].cantidad--;
+        localStorage.setItem("cartValues", JSON.stringify(productoCart));
+        renderCart();
+      }
+    });
+  });
+  //funcion eliminar
+  btnEliminar.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const index = e.target.getAttribute("data-index");
+      const productoCart = JSON.parse(localStorage.getItem("cartValues") || []);
+      productoCart.splice(index, 1);
+      localStorage.setItem("cartValues", JSON.stringify(productoCart));
+      renderCart();
+      finazarCompra();
+    });
+  });
+}
+function finazarCompra(params) {
+  const productosCart = JSON.parse(localStorage.getItem("cartValues")) || [];
+  const cartConfirm = document.querySelector("#jsFinCompra");
+  if (cartConfirm) {
+    cartConfirm.innerHTML = "";
+    if (productosCart.length === 0) {
+      const vacio = document.createElement("div");
+      vacio.classList.add("alert", "alert-info", "w-75", "text-center");
+      vacio.textContent = "El carrito está vacío.";
+      cartConfirm.appendChild(vacio);
+    } else {
+      const totalProd = productosCart.reduce(
+        (acc, item) => acc + item.cantidad,
+        0
+      );
+      const totalCompra = productosCart.reduce(
+        (acc, item) => acc + item.precio * item.cantidad,
+        0
+      );
+
+      const compra = document.createElement("div");
+      compra.classList.add("div-compra");
+      compra.innerHTML = `
+        <div class="card w-100 mb-3">
+          <div class="card-body">
+            <h5 class="card-title">Resumen de compra</h5>
+            <p class="card-text"><strong>Productos:</strong> ${totalProd}</p>
+            <p class="card-text"><strong>Total compra:</strong> $${totalCompra}</p>
+            <button class="btn btn-secondary btn-sm ">Confirmar</button>
+            <button class="btn btn-secondary btn-sm ">Cancelar</button>
+          </div>
+        </div>
+        `;
+      cartConfirm.appendChild(compra);
+      const btnConfirmar = compra.querySelector(".btn-confirmar");
+      const btnCancelar = compra.querySelector(".btn-cancelar");
+
+      btnConfirmar.addEventListener("click", () => {
+        alert("¡Compra confirmada! Gracias por tu compra.");
+        localStorage.removeItem("cartValues"); // Limpia el carrito
+        location.reload(); // Recarga la página
+      });
+
+      btnCancelar.addEventListener("click", () => {
+        alert("Compra cancelada.");
+        cartConfirm.innerHTML = ""; // Limpia el resumen
+      });
+    }
+  }
+}
+renderCart();
+finazarCompra();
